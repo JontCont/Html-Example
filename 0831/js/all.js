ca6197cfe48703a0b15daa091170b207aaedc104
JontCont <information9527@gmail.com>
2020-08-22 19:30:27 +0800

    0822

--- 0831/js/all.js
+++ 0831/js/all.js
@@ -4,3 +4,29 @@ function toggleMenu() {
     menuToggle.classList.toggle('active');
     header.classList.toggle('active');
 }
+
+var dY,
+    $win = $(window),
+    aniArr = ['ani-fadeIn-right', 'ani-fadeIn-right', 'ani-fadeIn-right'];
+
+$('.ani-fadeIn-up').eq(0).addClass('js-' + aniArr[0]);
+$('.ani-fadeIn-left').eq(0).addClass('js-' + aniArr[1]);
+$('.ani-fadeIn-right').eq(0).addClass('js-' + aniArr[2]);
+
+$win.scroll(function () {
+    var $this = $(this);
+    dY = $(this).scrollTop();
+
+    for (var i = 0, aniArrLen = aniArr.length; i < aniArrLen; i++) {
+        var items = $('.' + aniArr[i]);
+        for (var j = 1, itemLen = items.length; j < itemLen; j++) {
+            var $item = $(items[j]);
+            if (dY > $item.offset().top - $this.height() / 2) {
+                $item.addClass('js-' + aniArr[i]);
+            } else {
+                $item.removeClass('js-' + aniArr[i]);
+            }
+        }
+    }
+
+});
